aa3fe8a8317ef0fa90d11648cb53f9f7214d82c6
Caique Moraes <ike.moraes93@gmail.com>
2020-09-29 14:16:00 -0300

    Implementando padrao DDD

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express'
 import multer from 'multer'
-import uploadConfig from '../config/upload'
+import uploadConfig from '@config/upload'
 
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService'
-import CreateUserService from '../services/CreateUserService'
+
+import CreateUserService from '@modules/users/services/CreateUserService'
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
 const upload = multer(uploadConfig)
 
-import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 
 usersRouter.post('/', async (request, response) => {
   try {
